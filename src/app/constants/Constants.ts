e2dba0e0eec19d735fd453c045b7aeb43958bfd0
martleru <68188509+martleru@users.noreply.github.com>
2024-09-25 17:41:44 +0300

    add: Constants file

---
+++ src/app/constants/Constants.ts
@@ -0,0 +1,10 @@
+export const Constants = {
+  API_METHOD: {
+    GET_ALL_EMPLOYEES: 'GetAllEmployee',
+    GET_ALL_CLIENTS: 'GetAllClients',
+    GET_ALL_PROJECTS: 'GetAllClientProjects',
+  },
+  VALIDATION_MESSAGE: {
+    REQUIRED: 'This is required field',
+  },
+};
