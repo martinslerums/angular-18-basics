00a64160da7a0d30b56791a24549b93a0030dec4
martleru <68188509+martleru@users.noreply.github.com>
2024-09-25 17:41:17 +0300

    add: Reusable Component examples

---
+++ src/app/reusables/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() btnText: string = '';
+  @Input() btnClass: string = '';
+  
+  @Output() onBtnClicked = new EventEmitter();
+
+  onClick() {
+    this.onBtnClicked.emit()
+  }
+} 
